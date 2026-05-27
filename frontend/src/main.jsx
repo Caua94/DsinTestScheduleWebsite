171be7226f3f72207eaea66db3f567cd3c68d124
Caua94 <duarteps43@gmail.com>
2026-05-26 22:54:46 -0300

    Ajustes de docs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom' // Importa o provedor
-import { router } from './routes.jsx' // Importa as rotas que criamos
+import { RouterProvider } from 'react-router-dom' 
+import { router } from './routes.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
